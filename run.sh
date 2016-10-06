7c1545b84284fc90ca8c5b4fbba5a7fe904ffa6a
Richard Polzin <polzin.richard@gmail.com>
2016-10-06 08:14:50 +0200

    WIP

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-pdflatex image_processing.tex && bibtex image_processing.aux && pdflatex image_processing.tex && pdflatex image_processing.tex && evince image_processing.pdf
+pdflatex image_processing.tex && ./biber image_processing.bcf && pdflatex image_processing.tex && pdflatex image_processing.tex && evince image_processing.pdf
